35247f85a55e5f086650c01ec2a9abb6e41d53fd
Zainal Hasan <19884603+zhanang19@users.noreply.github.com>
2025-04-15 18:34:31 +0700

    feat: add simple pages

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <div>
+        Back to <Link href="/">Home</Link>
+      </div>
+    </div>
+  );
+}
